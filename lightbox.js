fdce2688965f716f9145d43eb2792decbaf75886
Jonno Clifford <jma.clifford@gmail.com>
2024-04-04 16:37:10 +0100

    image enlarge with trail resolved, JC images added

--- lightbox.js
+++ lightbox.js
@@ -1,20 +1,22 @@
 // Wait for the DOM content to be fully loaded
 document.addEventListener("DOMContentLoaded", function() {
-  // Fetch the image paths from the JSON file
-  fetch('images.json')
+  // Fetch the image data from the JSON file
+  fetch('images.json') // Assuming images.json contains the array of image objects
   .then(response => response.json())
   .then(data => {
-      const imageSrcArray = data.images;
+      const imagesArray = data.images; // Assuming data directly contains an array of image objects
 
-      // Shuffle the array of image sources
-      const shuffledImages = shuffleArray(imageSrcArray);
+      // Shuffle the array of image data
+      const shuffledImages = shuffleArray(imagesArray);
 
       const worldDiv = document.querySelector('.world');
 
-      // Create and append <img> elements for each image path
-      shuffledImages.forEach(imageSrc => {
+      // Create and append <img> elements for each image data
+      shuffledImages.forEach(imageData => {
           const img = document.createElement('img');
-          img.src = imageSrc;
+          img.src = imageData.src;
+          img.setAttribute('data-photographer', imageData.photographer); // Adding data attribute for photographer
+          img.setAttribute('data-camera', imageData.camera); // Adding data attribute for camera
           img.onclick = openLightbox;
           worldDiv.appendChild(img);
       });
@@ -37,7 +39,11 @@ function shuffleArray(array) {
 // Open lightbox with clicked image
 function openLightbox(event) {
   const imageSrc = event.target.src;
+  const photographer = event.target.dataset.photographer; // Getting photographer from data attribute
+  const camera = event.target.dataset.camera; // Getting camera from data attribute
+
   document.getElementById('lightboxImg').src = imageSrc;
+  document.getElementById('lightboxCaption').textContent = `Photographer: ${photographer}, Camera: ${camera}`; // Setting photographer and camera in the lightbox
   document.querySelector('.lightbox').style.display = 'block'; // Show the lightbox
 }
 
